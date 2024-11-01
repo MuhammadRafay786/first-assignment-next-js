534e1c974f1355504004d9ef880023a93a75ffcd
MuhammadRafay786 <muhammadhassnan85@gmail.com>
2024-10-31 22:31:27 -0700

    first commit

---
+++ .history/src/component/header_20241031120049.tsx
@@ -0,0 +1,7 @@
+import RenderFromTemplateContext from "next/dist/client/components/render-from-template-context";
+import    from "next/link";
+function header(params:any) {
+  <div className="flex">
+    <h1>hello</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus doloremque eligendi exercitationem nulla harum dignissimos dicta. Quia molestias quisquam nam reiciendis itaque distinctio temporibus, repellat nisi. Molestiae eos esse repellendus!</p></div>  
+}
